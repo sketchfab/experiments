fb7bf548ed22fb55727c53a002d2e2d858f8e1ac
Maurice Svay <maurice@sketchfab.com>
2015-10-28 18:45:05 +0100

    annotation-sync: Replaces camera polling by annotation API

--- annotations-sync/src/src/js/views/App.js
+++ annotations-sync/src/src/js/views/App.js
@@ -14,32 +14,10 @@ var AppView = Backbone.View.extend({
 
     initialize: function() {
 
+        this.currentAnnotation = null;
+
+        // Start the viewer
         this.initViewer(this.viewerReady.bind(this));
-
-        var current = new Backbone.Model();
-        current.on('change', function(model) {
-            var current = '#annotation-' + (1 + parseInt(this.get('order'), 10));
-            $('.slide').removeClass('active');
-            $(current).addClass('active');
-        });
-
-        this.listenTo(this, 'idle', function(e) {
-            if (e === true && this._annotations) {
-                _.each(this._annotations, function(annotation, i) {
-                    var distance = this.annotationDistance(annotation);
-                    if (distance === 0) {
-                        annotation.order = i;
-                        current.set(annotation);
-                    }
-                }.bind(this));
-            }
-        });
-
-        Backbone.ajax({
-            url: 'https://sketchfab.com/i/models/' + config.URLID + '/hotspots'
-        }).then(function(data) {
-            this._annotations = data.results;
-        }.bind(this));
 
     },
 
@@ -52,9 +30,9 @@ var AppView = Backbone.View.extend({
 
         client.init(urlid, _.extend({}, {
             success: function onSuccess(api) {
-                self._api = api;
                 api.start(function() {
                     api.addEventListener('viewerready', function() {
+                        self._api = api;
                         callback();
                     });
                 });
@@ -66,56 +44,20 @@ var AppView = Backbone.View.extend({
     },
 
     viewerReady: function() {
-        console.log('Viewer ready');
-        this.startCameraPolling();
+        // Update the current annotation when the annotation changes
+        this._api.addEventListener('annotationFocus', function(index) {
+            this.currentAnnotation = 1 + index;
+            this.render();
+        }.bind(this));
     },
 
-    startCameraPolling: function() {
-        this.timer = setInterval(function() {
-            this._api.getCameraLookAt(function(dummy, camera) {
-                this._camera = camera;
-                this._onCameraPolled();
-            }.bind(this));
-        }.bind(this), CAMERA_POLLING_INTERVAL);
-    },
-
-    _onCameraPolled: function() {
-        var camera = {
-            position: [
-                Math.round(this._camera.position[0] * CAMERA_THRESHOLD) / CAMERA_THRESHOLD,
-                Math.round(this._camera.position[1] * CAMERA_THRESHOLD) / CAMERA_THRESHOLD,
-                Math.round(this._camera.position[2] * CAMERA_THRESHOLD) / CAMERA_THRESHOLD,
-            ],
-            target: [
-                Math.round(this._camera.target[0] * CAMERA_THRESHOLD) / CAMERA_THRESHOLD,
-                Math.round(this._camera.target[1] * CAMERA_THRESHOLD) / CAMERA_THRESHOLD,
-                Math.round(this._camera.target[2] * CAMERA_THRESHOLD) / CAMERA_THRESHOLD,
-            ]
-        };
-        var signature = JSON.stringify(camera);
-        if (this.previousSignature && this.previousSignature === signature) {
-            this.trigger('idle', true);
-        } else {
-            this.previousSignature = signature;
-            this.trigger('idle', false);
+    render: function() {
+        if (this.currentAnnotation) {
+            var current = '#annotation-' + this.currentAnnotation;
+            $('.slide').removeClass('active');
+            $(current).addClass('active');
         }
-    },
-
-    annotationDistance: function(annotation) {
-        var ce = this._camera.position;
-        var ct = this._camera.target;
-        var cameraEye = vec3.fromValues(ce[0], ce[1], ce[2]);
-        var cameraTarget = vec3.fromValues(ct[0], ct[1], ct[2]);
-
-        var ae = annotation.eye;
-        var at = annotation.target;
-        var annotationEye = vec3.fromValues(ae[0], ae[1], ae[2]);
-        var annotationTarget = vec3.fromValues(at[0], at[1], at[2]);
-
-        var eyeDistance = vec3.distance(cameraEye, annotationEye);
-        var targetDistance = vec3.distance(cameraTarget, annotationTarget);
-
-        return eyeDistance + targetDistance;
+        return this;
     }
 });
 
