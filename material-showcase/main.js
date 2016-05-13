e92d4d2470a594f99b007e7e82e81403a522b996
Maurice Svay <maurice@sketchfab.com>
2016-05-13 16:09:27 +0200

    Adds material showcase

---
+++ material-showcase/main.js
@@ -0,0 +1,124 @@
+( function () {
+    'use strict';
+
+    // globals
+    var $ = window.$;
+
+    var model = 'https://sketchfab.com/models/93166cb1877f4895a91411334460898b/embed?autostart=1';
+    var materials = {};
+    var domain = 'https://sketchfab-labs.s3.amazonaws.com/';
+    var url = domain + '?prefix=materials';
+
+    function start() {
+        var html = '<iframe width="640" height="480" src="' + model + '" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>';
+
+        var iframe = html;
+        $( '.iframe' ).append( iframe );
+    }
+
+    function applyMaterial( id ) {
+        var iframeURL = model + '#' + materials[ id ].params;
+        $( 'iframe' ).attr( 'src', iframeURL );
+    }
+
+    function renderMaterialList() {
+        var out = '';
+        Object.keys( materials ).forEach( function ( materialName ) {
+            out += '<li class="material" data-material="' + materialName + '">' + materialName + '</li>';
+        });
+        $( '.materials' ).html(out);
+    }
+
+    function onLoadingStart() {
+        $('.loading').addClass('active');
+    }
+
+    function onLoadingStop() {
+        $('.loading').removeClass('active');
+    }
+
+    function fetchMaterials() {
+        return $.get( url )
+            .done( function ( materialListXML ) {
+                var filesXML = $( materialListXML ).find( 'Key' );
+                for ( var i = 0, l = filesXML.length; i < l; i++ ) {
+                    var element = filesXML[ i ];
+                    var file = $( element ).text();
+
+                    var split = file.split( '/' );
+
+                    // materials / materialName / file
+                    if ( split.length < 3 ) continue;
+
+                    var dir = split[ 1 ];
+                    if ( !materials[ dir ] ) {
+                        materials[ dir ] = [];
+                    }
+                    materials[ dir ].push( domain + file );
+                }
+
+                Object.keys( materials ).forEach( function ( materialName ) {
+                    var files = materials [ materialName ];
+                    var urlParams = [];
+                    var hasAO = false;
+                    var hasNormal = false;
+                    files.forEach( function ( file ) {
+                        if ( file.match( 'Base_Color' ) ) {
+                            urlParams.push( 'material_diffuse=' + file );
+                            return;
+                        }
+
+                        if ( file.match( 'Metallic' ) ) {
+                            urlParams.push( 'material_metalness=' + file );
+                            return;
+                        }
+
+                        if ( file.match( 'Ambient_Occlusion' ) ) {
+                            urlParams.push( 'material_cavity=' + file );
+                            hasAO = true;
+                            return;
+                        }
+
+                        if ( file.match( 'Normal' ) ) {
+                            urlParams.push( 'material_normal=' + file );
+                            hasNormal = true;
+                            return;
+                        }
+
+                        if ( file.match( 'Roughness' ) ) {
+                            urlParams.push( 'material_roughness=' + file );
+                            return;
+                        }
+
+                    } );
+
+                    if ( !hasAO )
+                        urlParams.push( 'material_cavity=' );
+                    if ( !hasNormal )
+                        urlParams.push( 'material_normal=' );
+
+                    materials [ materialName ][ 'params' ] = urlParams.join( ',' );
+                });
+
+                console.log( Object.keys( materials ).length + ' materials loaded');
+                console.log( materials);
+            });
+    }
+
+    $(function(){
+        $('.materials').on('click', '.material', function(e){
+            e.preventDefault();
+            var $target = $(e.currentTarget);
+            var materialId = $target.attr('data-material');
+            applyMaterial( materialId );
+            onLoadingStart();
+            setTimeout(onLoadingStop, 1000);
+        });
+
+        start();
+        fetchMaterials().then(function(){
+            renderMaterialList();
+        });
+    });
+
+} )();
