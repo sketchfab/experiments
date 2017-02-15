288130c81d3761ff743e821e77a37b2942679076
Maurice Svay <maurice@sketchfab.com>
2017-02-15 18:04:04 +0100

    Fixes URLs of video textures

--- video-textures/src/app.js
+++ video-textures/src/app.js
@@ -1,24 +1,24 @@
 window.materialName = 'Display';
 window.apps = [{
-    video: 'https://sketchfab.github.io/experiments/video-textures/videos/shazam.webm',
+    video: 'https://labs.sketchfab.com/experiments/video-textures/videos/shazam.webm',
     camera: {
         "position": [-5.20412822581436, -19.121745822133644, 3.154700374905264],
         "target": [0.000005142699927019269, -0.3250986610075804, 0.0890636007763596]
     }
 }, {
-    video: 'https://sketchfab.github.io/experiments/video-textures/videos/yahoo.webm',
+    video: 'https://labs.sketchfab.com/experiments/video-textures/videos/yahoo.webm',
     camera: {
         "position": [10.45378074877901, -14.66936406657716, 2.0479698873912184],
         "target": [0.000005142699927019269, -0.3250986610075804, 0.0890636007763596]
     }
 }, {
-    video: 'https://sketchfab.github.io/experiments/video-textures/videos/runtastic.webm',
+    video: 'https://labs.sketchfab.com/experiments/video-textures/videos/runtastic.webm',
     camera: {
         "position": [0.47380569188722443, -24.291117279406077, 1.7677300681241275],
         "target": [0.000005142699927019269, -0.3250986610075804, 0.0890636007763596]
     }
 }, {
-    video: 'https://sketchfab.github.io/experiments/video-textures/videos/facetune.webm',
+    video: 'https://labs.sketchfab.com/experiments/video-textures/videos/facetune.webm',
     camera: {
         "position": [6.362053080095855, -13.570428558563714, -7.621101955228509],
         "target": [-0.3669375256516855, 0.6024138807750593, -0.07880321051119216]
@@ -106,11 +106,3 @@ form.addEventListener('submit', function(e){
         });
     }
 }, false);
-
-
-// var btGetCamera = document.querySelector('.getcamera');
-// btGetCamera.addEventListener('click', function() {
-//     api.getCameraLookAt(function(err, camera) {
-//         console.log(JSON.stringify(camera));
-//     });
-// }, false);
