39e919303eec63bdc7366f837d94c126de62700c
Maurice Svay <maurice@svay.com>
2018-04-10 14:18:41 +0200

    Updates configurator experiment
    * fixes UI
    * removes Backbone and build step
    * adds info and links

--- configurator/src/src/js/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var $ = require('jquery');
-var _ = require('underscore');
-var Backbone = require("backbone");
-Backbone.$ = $;
-var AppView = require('./views/App');
-
-var appView = new AppView();
--- configurator/src/src/js/config.js
+++
@@ -1,6 +0,0 @@
-var config = {
-    URLID: 'c632823b6c204797bd9b95dbd9f53a06',
-    OPTION_PREFIX : 'seat '
-};
-
-module.exports = config;
